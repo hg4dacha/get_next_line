35ad76bce90298e6784d6d3d4bef0306069e4f23
Honsse GADACHA <hgadacha@e3r13p10.42.fr>
2019-10-13 18:37:53 +0200

    Correction lecture ligne simple

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hgadacha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/15 11:24:23 by hgadacha          #+#    #+#             */
-/*   Updated: 2019/10/13 13:11:21 by hgadacha         ###   ########.fr       */
+/*   Updated: 2019/10/13 18:36:25 by hgadacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,28 @@ int						ft_strnlen(char *str, char c)
 	return (j);
 }
 
-int						ft_affect(int i, char **dest, char **line)
+int						ft_affect(char **dest, char **line)
 {
+	int					i;
+
 	i = ft_strnlen((*dest), '\n');
-	(*line) = ft_strsub(*dest, 0, i);
-	if (*line == NULL)
+	if (!((*line) = ft_strsub(*dest, 0, i)))
 		return (-1);
 	if ((*dest)[i] != '\0')
 		(*dest) = ft_strcpy((*dest), ((*dest) + i) + 1);
 	else
-		free(dest);
+		ft_strdel(dest);
 	return (1);
 }
 
 int						get_next_line(const int fd, char **line)
 {
 	int					read_return;
-	char				stock[BUFF_SIZE + 1];
 	static char			*dest[OPEN_MAX];
-	int					i;
+	char				stock[BUFF_SIZE + 1];
 	char				*tmp;
 
-	i = 0;
-	if (fd < 3 || fd > OPEN_MAX || line == NULL)
+	if (fd < 0 || fd > OPEN_MAX || line == NULL)
 		return (-1);
 	if (dest[fd] == NULL)
 		if (!(dest[fd] = (char*)malloc(sizeof(char))))
@@ -53,12 +52,13 @@ int						get_next_line(const int fd, char **line)
 	{
 		stock[read_return] = '\0';
 		tmp = dest[fd];
-		dest[fd] = ft_strjoin(dest[fd], stock);
+		if (!(dest[fd] = ft_strjoin(dest[fd], stock)))
+			return (-1);
 		free(tmp);
 		if (ft_strchr(dest[fd], '\n') != NULL)
 			break ;
 	}
 	if (read_return <= 0 && ft_strlen(dest[fd]) == 0)
 		return (read_return);
-	return (ft_affect(i, &dest[fd], line));
+	return (ft_affect(&dest[fd], line));
 }
